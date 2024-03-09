2e6b547abe1cc3c36e50801da3d10bb60ea376bb
akanna1218 <ajithkannapandian@gmail.com>
2024-03-09 13:39:57 +0530

    made responsive to mobile

--- src/Functions/RegexValidator.jsx
+++ src/Functions/RegexValidator.jsx
@@ -1,12 +1,19 @@
 //emailRegex = /^[^\s@]+@[^\s@]+$/;
-// passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+// The above Regex format will check the  given input is in email format.
+
 
 export const emailValidator=(i)=>{
     const emailRegex = /^[^\s@]+@[^\s@]+$/;
     return emailRegex.test(i)
 }
 
+
+
 export const passWordValidator=(j)=>{
 const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
 return passwordRegex.test(j)
 }
+
+
+// passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+// The above Regex format will check the  given password has a capital and a small letter , a special character , a number and minimum length of 8.
